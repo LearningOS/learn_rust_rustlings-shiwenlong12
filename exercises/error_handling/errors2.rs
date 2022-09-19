46c77495fc031183c27e299015c36c1ec87fe44b
shiwenlong12 <105833127+shiwenlong12@users.noreply.github.com>
2022-09-19 03:30:12 +0000

    rustlings

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -24,8 +24,12 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
+    match qty.kind() {
+        QtyKind:: => Err("invalid digit found in string"),
+        _ => Ok(item_quantity * cost_per_item + processing_fee),
+    };
 
-    Ok(qty * cost_per_item + processing_fee)
+    
 }
 
 #[cfg(test)]
